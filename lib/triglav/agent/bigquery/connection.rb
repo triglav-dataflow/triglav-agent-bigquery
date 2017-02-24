8e61a3e67cdd15e6dcbf9526150332b4403e6dc0
sonots <sonots@gmail.com>
2017-02-24 16:00:36 +0900

    auth

--- lib/triglav/agent/bigquery/connection.rb
+++ lib/triglav/agent/bigquery/connection.rb
@@ -34,16 +34,73 @@ module Triglav::Agent
         @connection
       end
 
-      def set_resource_pool
-        if @connection_info[:resource_pool] and !@connection_info[:resource_pool].empty?
-          @connection.query("set session resource_pool = '#{@connection_info[:resource_pool]}'")
-        end
+      # compute_engine, authorized_user, service_account
+      def auth_method
+        @auth_method ||= ENV['AUTH_METHOD'] || @connection_info.fetch(:auth_method, nil) || credentials['type'] || 'compute_engine'
       end
 
-      def set_memorycap
-        if @connection_info[:memorycap] and !@connection_info[:memorycap].empty?
-          @connection.query("set session memorycap = '#{@connection_info[:memorycap]}'")
-        end
+      def credentials
+        JSON.parse(@connection_info.fetch(:credentials, nil) || File.read(credentials_file))
+      end
+
+      def credentials_file
+        @credentials_file ||= File.expand_path(
+          # ref. https://developers.google.com/identity/protocols/application-default-credentials
+          ENV['GOOGLE_APPLICATION_CREDENTIALS'] ||
+          @connection_info.fetch(:credentials_file, nil) ||
+          (File.exist?(global_application_default_credentials_file) ? global_application_default_credentials_file : application_default_credentials_file)
+        )
+      end
+
+      def application_default_credentials_file
+        @application_default_credentials_file ||= File.expand_path("~/.config/gcloud/application_default_credentials.json")
+      end
+
+      def global_application_default_credentials_file
+        @global_application_default_credentials_file ||= '/etc/google/auth/application_default_credentials.json'
+      end
+
+      def config_default_file
+        File.expand_path('~/.config/gcloud/configurations/config_default')
+      end
+
+      def config_default
+        # {'core'=>{'account'=>'xxx','project'=>'xxx'},'compute'=>{'zone'=>'xxx}}
+        @config_default ||= File.readable?(config_default_file) ? IniFile.load(config_default_file).to_h : {}
+      end
+
+      def service_account_default
+        (config_default['core'] || {})['account']
+      end
+
+      def project_default
+        (config_default['core'] || {})['project']
+      end
+
+      def zone_default
+        (config_default['compute'] || {})['zone']
+      end
+
+      def project
+        @project ||= ENV['GOOGLE_PROJECT'] || @connection_info.fetch(:project, nil) || credentials['project_id']
+        @project ||= credentials['client_email'].chomp('.iam.gserviceaccount.com').split('@').last if credentials['client_email']
+        @project ||= project_default
+      end
+
+      def service_account
+        @service_account ||= ENV['GOOGLE_SERVICE_ACCOUNT'] || @connection_info.fetch(:service_account, nil) || credentials['client_email'] || service_account_default
+      end
+
+      def retries
+        @retries ||= ENV['RETRIES'] || @connection_info.fetch(:retries, 5)
+      end
+
+      def timeout_sec
+        @timeout_sec ||= ENV['TIMEOUT_SEC'] || @connection_info.fetch(:timeout_sec, 300)
+      end
+
+      def open_timeout_sec
+        @open_timeout_sec ||= ENV['OPEN_TIMEOUT_SEC'] || @connection_info.fetch(:open_timeout_sec, 300)
       end
     end
   end
--- triglav-agent-bigquery.gemspec
+++ triglav-agent-bigquery.gemspec
@@ -19,10 +19,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "bigquery"
   spec.add_dependency "triglav-agent"
   spec.add_dependency "triglav_client"
-  spec.add_dependency "rack" # Rack::Utils
+  spec.add_dependency "google-api-client"
+  spec.add_dependency "ini_file"
 
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
