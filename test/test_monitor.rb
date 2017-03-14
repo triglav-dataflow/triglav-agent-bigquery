5f075b5525cf6327600e9e247e5ce54d49073b8a
sonots <sonots@gmail.com>
2017-03-14 13:22:57 +0900

    fix tests to pass

--- test/test_monitor.rb
+++ test/test_monitor.rb
@@ -57,6 +57,10 @@ if File.exist?(File.join(ROOT, '.env'))
       }.merge(params))
     end
 
+    def resource_uri_prefix
+      'https://bigquery.cloud.google.com'
+    end
+
     def test_resource_valid
       resource = build_resource(unit: 'singular,daily,hourly')
       assert { Monitor.resource_valid?(resource) == false }
@@ -76,7 +80,7 @@ if File.exist?(File.join(ROOT, '.env'))
 
     def test_project_dataset_table
       resource = build_resource(unit: 'singular')
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       assert { monitor.send(:project_dataset_table) == "#{project}:#{dataset}.#{table}" }
       assert { monitor.send(:project) == project }
       assert { monitor.send(:dataset) == dataset }
@@ -85,13 +89,13 @@ if File.exist?(File.join(ROOT, '.env'))
 
     def test_process
       resource = build_resource
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       assert_nothing_raised { monitor.process }
     end
 
     def test_get_hourly_events
       resource = build_resource(unit: 'hourly')
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       success = monitor.process do |events|
         assert { events != nil}
         # assert { events.size == resource.span_in_days * 24 }
@@ -106,7 +110,7 @@ if File.exist?(File.join(ROOT, '.env'))
 
     def test_get_daily_events
       resource = build_resource(unit: 'daily')
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       success = monitor.process do |events|
         assert { events != nil}
         assert { events.size == resource.span_in_days }
@@ -121,7 +125,7 @@ if File.exist?(File.join(ROOT, '.env'))
 
     def test_get_singular_events
       resource = build_resource(unit: 'singular')
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       success = monitor.process do |events|
         assert { events != nil}
         assert { events.size == 1 }
@@ -136,7 +140,7 @@ if File.exist?(File.join(ROOT, '.env'))
 
     def test_get_daily_events_for_partitioned_table
       resource = build_resource(unit: 'daily', partitioned: true)
-      monitor = Monitor.new(connection, resource)
+      monitor = Monitor.new(connection, resource_uri_prefix, resource)
       success = monitor.process do |events|
         assert { events != nil}
         assert { events.size == resource.span_in_days }
