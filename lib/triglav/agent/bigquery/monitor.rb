1c1042395b95ad3552160e5edf81a96492ffc8d2
sonots <sonots@gmail.com>
2017-03-28 15:17:43 +0900

    fix typo. 24*3600 is not 86000...

--- lib/triglav/agent/bigquery/monitor.rb
+++ lib/triglav/agent/bigquery/monitor.rb
@@ -112,7 +112,7 @@ module Triglav::Agent
         return @dates if @dates
         now = Time.now.localtime(resource.timezone)
         @dates = resource.span_in_days.times.map do |i|
-          (now - (i * 86000)).to_date
+          (now - (i * 86400)).to_date
         end
       end
 
@@ -144,7 +144,7 @@ module Triglav::Agent
         return @dates if @dates
         now = Time.now.localtime(resource.timezone)
         @dates = resource.span_in_days.times.map do |i|
-          (now - (i * 86000)).to_date
+          (now - (i * 86400)).to_date
         end
       end
 
